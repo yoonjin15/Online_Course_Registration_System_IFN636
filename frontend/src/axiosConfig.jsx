eba167cee7fd01f87fe3c3168a13896af8859821
Yoonjin Ahn <ayj9701@naver.com>
2026-04-03 11:49:54 +0000

    fix: update axios baseURL to use EC2 server

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://3.26.96.188:5001', // live
+  //baseURL: 'http://13.236.187.203:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
